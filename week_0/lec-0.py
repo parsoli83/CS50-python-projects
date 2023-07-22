b9ea7d2f67c59aca8b4ac80abebe4279864c1a9c
parsoli83 <mr.parsoli@gmail.com>
2023-07-22 10:43:32 +0330

    lec-0 finished

--- week_0/lec-0.py
+++ week_0/lec-0.py
@@ -18,7 +18,7 @@ print("world")
 helloworld
 """
 
-# the string can be have and extra space or a , after it
+# the string can extra space or a , after it
 
 #input function didnt have many things about it in python documents
 
@@ -53,7 +53,7 @@ this variable contains stuff
 1-add f before the string to make format string
 2-put variabe in {}
 
-tou can have functions and even other stuff here too
+you can have functions and even other stuff here too
 
 def cat():
     return "niaaa"
@@ -109,4 +109,79 @@ print(stri.split("@"))
 """
 
 
+"""
 
+***int***
+
++   plus
+-   negative
+/   divide
+%   remainder   
+*   multiply
+**  power
+
+"""
+
+"""
+
+***float***
+
+#like 1.23 or 4.56
+
+=> float(thing) #turns it into a floating point
+
+=> round(number[, ndigits]) #gives the nearest number with optional number of decimals
+
+print(round(1.23456, 4))
+1.2346
+
+
+#f-string stuff
+
+>>> z=1.222+2.333
+>>> print(f"{z}")
+3.555
+>>> print(f"{z:,}")     #this will make numbers more readable
+3.555
+>>> print(f"{z:.2f}")   #this will only show two digits by rounding up
+3.56
+
+"""
+
+"""
+
+***functions/def***
+
+def name _of_the_function([*arguments]):
+    code
+    return [*parameters]
+
+>>> def street(name,vehicle,number):
+...     print(f"{number} {vehicle} crossed the {name} street.")
+... 
+
+>>> street("street","car",6)
+6 car crossed the street street.
+
+***defaut value***
+
+#to give a default value just put = and the default value after variable
+
+>>> def street(name=goli,vehicle=tram,number=10):
+...     print(f"{number} {vehicle} crossed the {name} street.")
+... 
+
+>>> street()
+10 tram crossed the goli street.
+
+#you can return stuff to
+
+def sum(a,b):
+    return a+b
+print(sum(1,2))
+
+***scope***
+
+variables can be global or only in a function
+
+"""
