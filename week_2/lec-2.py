28af84c130e5457a3d0ebd8ba63e4baef3304ce8
parsoli83 <mr.parsoli@gmail.com>
2023-08-26 17:58:16 +0330

    lecture 2 finished

---
+++ week_2/lec-2.py
@@ -0,0 +1,61 @@
+"""
+
+***loop***
+
+while
+for
+
+while bool:
+    do shit or break
+
+while True:    #infinite loop
+
+***loop arguements***
+
+continue    #keep going
+break   #breaks out of loop
+i+=1 == i=i+1 
+
+***list***
+
+a data storage method
+
+list=[1,2,3,4,5]
+
+len(list)   #length of a list
+
+***dictionary(map/table)***
+
+dic={
+    "a":"good",
+    "b":"normal",
+    "c":"bad"
+}
+print(dic["a"])
+
+for grade in dic:
+    print(grade,dic[grade])
+
+#a for loop in a dictionary gives the keys (a,b,c)
+
+###pythonic feature###
+
+if you ony declare a variable for the code to function but dont intend to use it:
+
+name it _   (underscore)
+
+for _in range(9):
+    print("yo")
+
+### weird feature ###
+
+print("yo\n"*3, end="")
+
+yo
+yo
+yo
+
+###None###
+None    #official abcense of value 
+
+"""
