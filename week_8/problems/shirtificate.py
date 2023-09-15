8796d358fdb65275f1a82551a4366085c5d4b77b
parsoli83 <mr.parsoli@gmail.com>
2023-09-15 12:06:41 +0330

    added the certificate

--- week_8/problems/shirtificate.py
+++ week_8/problems/shirtificate.py
@@ -5,8 +5,8 @@ page.set_font("helvetica", "B", 60)
 page.cell(txt="CS50 Shirtificate",align='C',center=True)
 a4_height=297
 a4_width=210
-page.image('shirtificate.png', x= 20, y= 80, w=170)
+page.image('shirtificate.png', x= 10, y= 80, w=190)
 page.set_text_color(255,255,255)
-page.set_font("helvetica", "B", 20)
+page.set_font("helvetica", "B", 30)
 page.cell(txt=f"{input('Name: ')} took CS50",h=250,align='C',center=True)
 page.output("shirtificate.pdf")
